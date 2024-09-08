b5db25240b400684785228442985826291f92008
Hersey <helane1996@gmail.com>
2024-09-08 19:37:17 -0400

    Added home page integration

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -4,6 +4,7 @@ import '../styles/index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
 
 const router = createBrowserRouter([
   {
--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,2 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Link, Navigate } from "react-router-dom";
+
+const Home = () => {
+
+    return (
+        <div>
+            <h1>Hersey Lane's Portfolio</h1>
+        </div>
+    );
+};
+
+export default Home
